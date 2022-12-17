2f5a029828c02061e19c9f0023b0e5f4f3a18223
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-12-17 14:03:12 -0600

    Redo all the code

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function makeBookmarklet() {
-  const bookmarkletCode = document.getElementById("bookmarklet").value;
+  const bookmarkletCode = document.querySelector("#bookmarklet").value;
   const subject = document.querySelector("#parent");
-  const bookmarklet = `(function(){${bookmarkletCode}})()`;
-  const final = encodeURI(bookmarklet);
+  const bookmarklet = `javascript:(function(){${bookmarkletCode}})()`;
+  const [prefix, code] = bookmarklet.split(":");
+  const final = `${prefix}:${encodeURIComponent(code)}`;
   console.log(final);
   subject.insertAdjacentHTML(
     "afterbegin",
-    "<p>Your bookmarklet code: <code>" + final + "</code></p>"
+    `<p>Your bookmarklet code: <code>${final}</code></p>`
   );
 }
-
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function() {
+  const parentDiv = document.querySelector("#parent");
+  parentDiv.innerHTML = "";
+});
