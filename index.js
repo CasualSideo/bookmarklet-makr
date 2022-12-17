4e72fccec6988214fa41e74256b9341bb2eacf3c
Gabe <66077254+MrBrain295@users.noreply.github.com>
2022-12-17 15:05:19 -0600

    simplify

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 function makeBookmarklet() {
-  const bookmarkletCode = document.querySelector("#bookmarklet").value;
+  const code = document.querySelector("#bookmarklet").value;
   const subject = document.querySelector("#parent");
-  const bookmarklet = `javascript:(function(){${bookmarkletCode}})()`;
-  const [prefix, code] = bookmarklet.split(":");
-  const final = `${prefix}:${encodeURIComponent(code)}`;
-  console.log(final);
-  subject.insertAdjacentHTML(
-    "afterbegin",
-    `<p>Your bookmarklet code: <code>${final}</code></p>`
-  );
+  const bookmarklet = `javascript:(function(){${code}})()`;
+  const final = `javascript:${encodeURIComponent(bookmarklet)}`;
+  subject.innerHTML = `<p>Your bookmarklet code: <code>${final}</code></p>
+                      <a href="${final}">Click here to use your bookmarklet</a>`;
 }
-const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", function() {
-  const parentDiv = document.querySelector("#parent");
-  parentDiv.innerHTML = "";
+
+document.querySelector("#reset").addEventListener("click", () => {
+  document.querySelector("#parent").innerHTML = "";
 });
