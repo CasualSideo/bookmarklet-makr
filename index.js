44cd182ad962c332cefecff95f6f4f4e32ef5cbe
Gabriel Whigham <gabewhigham@gmail.com>
2022-12-18 15:20:33 -0600

    better

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function makeBookmarklet() {
-  const code = document.querySelector("#bookmarklet").value;
+async function makeBookmarklet() {
+  const code = document.querySelector("#bookmarklet");
   const subject = document.querySelector("#parent");
-  const bookmarklet = `javascript:(function(){${code}})()`;
-  const final = `javascript:${encodeURIComponent(bookmarklet)}`;
-  subject.innerHTML = `<p>Your bookmarklet code: <code>${final}</code></p>
-                      <a href="${final}">Click here to use your bookmarklet</a>`;
+
+  // Use template literals to create the bookmarklet code
+  const bookmarklet = `(function(){${code}})()`;
+  const final = encodeURIComponent(bookmarklet);
+
+  // Use template literals to create the HTML elements
+  subject.innerHTML = `
+    <p>Your bookmarklet code: <code>javascript:${final}</code></p>
+    <a href="javascript:${final}">Click here to use your bookmarklet</a>
+  `;
 }
 
 document.querySelector("#reset").addEventListener("click", () => {
